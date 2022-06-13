ff25155eaf8ed32a4b8a44020aae5d20631b7fd2
Franklin Chua <franklin.chua@fireclay.ph>
2022-06-13 21:01:00 +0800

    Test parsing of PDF document

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,25 @@
 module Main where
 
-import HaskellSay (haskellSay)
+import Pdf.Core.File hiding (withPdfFile)
+import Pdf.Document
+
+processMe :: Pdf -> IO ()
+processMe pdf = do
+    encrypted <- isEncrypted pdf
+    print ("File is encrypted: " ++ show encrypted)
+    doc <- document pdf
+    catalog <- documentCatalog doc
+    rootNode <- catalogPageNode catalog
+    count <- pageNodeNKids rootNode
+    print ("No. of pages: " ++ show count)
+    page <- pageNodePageByNum rootNode 1
+    fonts <- pageFontDicts page
+    let fontNames = map (\x -> fst x) fonts
+    glyphs <- pageExtractGlyphs page
+    let text = glyphsToText glyphs
+    print $ "End: " ++ (show fontNames)
+    print $ text
 
 main :: IO ()
-main = 
-    haskellSay "Hello, Haskell!"
+main =
+    withPdfFile "Price-Monitoring-June-10-2022.pdf" processMe
